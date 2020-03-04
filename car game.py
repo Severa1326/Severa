03bf12b924a9928deee31efc7dbf1899f1372550
Severa1326 <61756046+Severa1326@users.noreply.github.com>
2020-03-03 22:25:20 -0400

    Update car game.py

--- car game.py
+++ car game.py
@@ -1,3 +1,4 @@
+"ESTE ES EL QUE EL TIPO DEL VIDEO HIZO"
 print("Hello this is a car game. To start, try by typing 'help'")
 user_input = ""
 started = False
@@ -27,4 +28,33 @@ quit - to exit the game""")
         print("that's an input that I do not understand. Try typing 'help'")
         
 "done 03/03/2020"
+
+"DONE BY ME"
+print("This is a car game, start by typing 'help': ")
+command = ""
+car_has_started = False
+while command != "quit":
+    command = input("> ").lower()
+    if command == "start":
+        if car_has_started:
+            print("Car has already started")
+        else:
+            car_has_started = True
+            print("Car has started")
+    elif command == "stop":
+        if not car_has_started:
+            print("Car has already stopped")
+        else:
+            car_has_started = False
+            print("Car has stopped")
+    elif command == "help":
+        print("""
+start - car starts
+stop - car stops
+quit - quits the game""")
+    elif command == "quit":
+        print("Thank you for playing")
+        break
+    else:
+        print("Please type 'help' for instructions")
 
